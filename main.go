e072f5cb68c708b3bd983fa5266160578f9d913b
gmatteo <matteo.grange@ynov.com>
2025-12-08 17:45:01 +0100

    ajout location / dates / relation api handlers

--- main.go
+++ main.go
@@ -18,6 +18,34 @@ type Artist struct {
     FirstAlbum   string   `json:"firstAlbum"`
 }
 
+type Locations struct {
+    Index []LocationItem `json:"index"`
+}
+
+type LocationItem struct {
+    ID int `json:"id"`
+    Locations []string `json:"locations"`
+    Dates string `json:"dates"`
+}
+
+type Dates struct {
+    Index []DatesItem `json:"index"`
+}
+
+type DatesItem struct {
+    ID int `json:"id"`
+    Dates []string `json:"dates"`
+}
+
+type Relation struct {
+    Index []RelationItem `json:"index"`
+}
+
+type RelationItem struct {
+    ID             int                 `json:"id"`
+    DatesLocations map[string][]string `json:"datesLocations"` //revoir les maps (pas bien compris "map[string][]string")
+}
+
 var tmplHome    = template.Must(template.ParseFiles("templates/home.html"))
 var tmplArtists = template.Must(template.ParseFiles("templates/artists.html"))
 var tmplLocations = template.Must(template.ParseFiles("templates/locations.html"))
@@ -29,9 +57,6 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func artistsHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("artistsHandler called")
-
-
     url := "https://groupietrackers.herokuapp.com/api/artists"
 
     response, err := http.Get(url)
@@ -60,17 +85,88 @@ func artistsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func locationsHandler(w http.ResponseWriter, r *http.Request) {
-    tmplLocations.Execute(w, nil)
+        url := "https://groupietrackers.herokuapp.com/api/locations"   
+
+    response, err := http.Get(url)
+    if err != nil {
+        http.Error(w, "Erreur lors de la requête HTTP", http.StatusInternalServerError)
+        return
+    }
+    defer response.Body.Close()
+    
+    body, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusInternalServerError)
+        return
+    }
+
+        var data Locations
+    if err := json.Unmarshal(body, &data); err != nil {
+        http.Error(w, "Erreur de décodage JSON", http.StatusInternalServerError)
+        return
+    }
+
+    if err := tmplLocations.Execute(w, data); err != nil {
+        http.Error(w, "Erreur d’exécution du template", http.StatusInternalServerError)
+        return
+    }
 }
 
 func datesHandler(w http.ResponseWriter, r *http.Request) {
-    tmplDates.Execute(w, nil)
+url := "https://groupietrackers.herokuapp.com/api/dates"
+
+    response, err := http.Get(url)
+    if err != nil {
+        http.Error(w, "Erreur lors de la requête HTTP", http.StatusInternalServerError)
+        return
+    }
+    defer response.Body.Close()
+
+    body, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusInternalServerError)
+        return
+    }
+
+            var data Dates
+    if err := json.Unmarshal(body, &data); err != nil {
+        http.Error(w, "Erreur de décodage JSON", http.StatusInternalServerError)
+        return
+    }
+
+    if err := tmplDates.Execute(w, data); err != nil {
+        http.Error(w, "Erreur d’exécution du template", http.StatusInternalServerError)
+        return
+    }
 }
 
 func relationHandler(w http.ResponseWriter, r *http.Request) {
-    tmplRelation.Execute(w, nil)
-}
+    url := "https://groupietrackers.herokuapp.com/api/relation"
 
+        response, err := http.Get(url)
+    if err != nil {
+        http.Error(w, "Erreur lors de la requête HTTP", http.StatusInternalServerError)
+        return
+    }
+    defer response.Body.Close()
+
+        body, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusInternalServerError)
+        return
+    }
+
+        var data Relation
+    if err := json.Unmarshal(body, &data); err != nil {
+        http.Error(w, "Erreur de décodage JSON", http.StatusInternalServerError)
+        return
+    }
+
+        if err := tmplRelation.Execute(w, data); err != nil {
+        http.Error(w, "Erreur d’exécution du template", http.StatusInternalServerError)
+        return
+    }
+}
 
 
 func main() {
